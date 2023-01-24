b4545542a01a41439eb765b4a5f735a252a14d34
Drunkiel <drunkielfel@gmail.com>
2023-01-24 10:42:31 +0100

    Added check if is enough energy

--- Assets/Scripts/Player/PlayerSkillsController.cs
+++ Assets/Scripts/Player/PlayerSkillsController.cs
@@ -4,15 +4,37 @@ public class PlayerSkillsController : MonoBehaviour
 {
     public bool isLightOn;
     public bool isNightVisionOn;
+    public bool isEnoughEnergy;
 
     public GameObject flashLight;
     public GameObject nightVisionLight;
 
+    PlayerStatsController _statsController;
+
+    void Start()
+    {
+        _statsController = GetComponent<PlayerStatsController>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)) ChangeLightState();
-        if (Input.GetKeyDown(KeyCode.E)) ChangeVisionState();
+        if (CheckIfEnoughEnergy())
+        {
+            if (Input.GetMouseButtonDown(1)) ChangeLightState();
+            if (Input.GetKeyDown(KeyCode.E)) ChangeVisionState();
+        }
+        else
+        {
+            if (isLightOn) ChangeLightState();
+            if (isNightVisionOn) ChangeVisionState();
+        }
+    }
+
+    bool CheckIfEnoughEnergy()
+    {
+        if (_statsController.energyReserve > 0) return true;
+        else return false;
     }
 
     void ChangeLightState()
