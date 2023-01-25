cb34dc23d6e83a396f8276004e3bff0774500d8d
Drunkiel <drunkielfel@gmail.com>
2023-01-25 10:46:54 +0100

    Fixed infinity range

---
+++ Assets/Scripts/InteractiveObjects/ButtonObject.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonObject : MonoBehaviour
+{
+    public GameObject cursorToDisplay;
+    public bool onClick;
+    public bool onDrag;
+    public UnityEvent unityEvent;
+
+    TriggerController _triggerController;
+
+    void Start()
+    {
+        _triggerController = GetComponent<TriggerController>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (_triggerController.isTriggered) cursorToDisplay.SetActive(true);
+        else cursorToDisplay.SetActive(false);
+    }
+
+    void OnMouseExit()
+    {
+        cursorToDisplay.SetActive(false);
+    }
+
+    void OnMouseDrag()
+    {
+        if (onDrag && _triggerController.isTriggered) unityEvent.Invoke();
+    }
+
+    void OnMouseDown()
+    {
+        if (onClick && _triggerController.isTriggered) unityEvent.Invoke();
+    }
+}
