e3dc75c82515d244f33b2b2f8ad25801d6f9585d
Drunkiel <drunkielfel@gmail.com>
2023-01-30 17:58:52 +0100

    Added saving and loading save

---
+++ Assets/Scripts/SaveLoad/PlayerData.cs
@@ -0,0 +1,7 @@
+[System.Serializable]
+public class PlayerData
+{
+    public int coins;
+    public int energyPoints;
+    public int chargingPoints;
+}
---
+++ Assets/Scripts/SaveLoad/SaveLoadController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.IO;
+
+public class SaveLoadController : MonoBehaviour
+{
+    private string jsonSavePath;
+    public PlayerData _playerData;
+    PlayerStatsController _playerStats;
+
+    void Awake()
+    {
+        jsonSavePath = Application.persistentDataPath + "/Data.json";
+        _playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatsController>();
+
+        try
+        {
+            Load();
+        }
+        catch (System.Exception)
+        {
+            Save();
+        }
+    }
+
+    public void Save()
+    {
+        //Creating file
+        FileStream File1 = new FileStream(jsonSavePath, FileMode.OpenOrCreate);
+
+        //Data to save
+        _playerData.coins = _playerStats.coins;
+        _playerData.energyPoints = PlayerUpgrades.energyPoints;
+        _playerData.chargingPoints = PlayerUpgrades.fastChargingPoints;
+
+        //Saving data
+        string jsonData = JsonUtility.ToJson(_playerData, true);
+
+        File1.Close();
+        File.WriteAllText(jsonSavePath, jsonData);
+    }
+
+    public void Load()
+    {
+        //Loading data
+        string json = ReadFromFile();
+        JsonUtility.FromJsonOverwrite(json, _playerData);
+
+        _playerStats.coins = _playerData.coins;
+        PlayerUpgrades.energyPoints = _playerData.energyPoints;
+        PlayerUpgrades.fastChargingPoints = _playerData.chargingPoints;
+    }
+
+    private string ReadFromFile()
+    {
+        using (StreamReader Reader = new StreamReader(jsonSavePath))
+        {
+            string json = Reader.ReadToEnd();
+            return json;
+        }
+    }
+}
